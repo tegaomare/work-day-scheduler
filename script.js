e5da1dad5f249662fea49b2518c38c34fa6f2322
Tegaomare <tegaomare@oghenetegasimac.mynetworksettings.com>
2023-05-28 19:54:57 -0400

    added a click event on save button

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-//$(function () {
+//$(document).ready(function () {
+  // Get the current date and display it in the header
+  var currentDate = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(currentDate);
+
+  // Update the time-block classes based on the current hour
+  updateHourlyBlocks();
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
 // function? How can DOM traversal be used to get the "hour-x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
-//
+//$(".saveBtn").on("click", function () {
+  var timeBlock = $(this).parent();
+  var hour = timeBlock.attr("id");
+  var text = timeBlock.find(".description").val();
+
+  // Save the text to local storage
+  localStorage.setItem(hour, text);
+});
 // TODO: Add code to apply the past, present, or future class to each time
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
